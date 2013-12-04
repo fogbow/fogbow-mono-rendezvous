3cbc07df7b7f71ed37e690fd6aed842187ba4ab0
leticiamaia <leticiamt@hotmail.com>
2013-12-04 10:53:05 -0300

    Adding a testImAliveNullParameter method to RendezvousTest class

--- src/test/java/org/ourgrid/cloud/RendezvousTest.java
+++ src/test/java/org/ourgrid/cloud/RendezvousTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.rules.ExpectedException;
 import org.ourgid.cloud.Rendezvous;
 import org.ourgid.cloud.RendezvousImpl;
 
@@ -13,83 +15,64 @@ public class RendezvousTest {
 	public void testImAliveSingleElement() {
     	Rendezvous r = new RendezvousImpl();
     	r.iAmAlive("abc");
-    	List<String> valor = r.whoIsAlive();
-    	Assert.assertEquals(1, valor.size());
-    	Assert.assertEquals("abc", valor);
+    	List<String> Element = r.whoIsAlive();
+    	Assert.assertEquals(1, Element.size());
+    	Assert.assertEquals("abc", Element);
 	}
     
     @Test
-    public void testImAlive2Elements() {
+    public void testImAliveEqualElements() {
     	Rendezvous r = new RendezvousImpl();
-    	r.iAmAlive("abc");
-    	r.iAmAlive("leticia");
-    	List<String> valor = r.whoIsAlive();
-    	Assert.assertEquals(2, valor.size());
-    	Assert.assertEquals("abc", valor.get(0));
-    	Assert.assertEquals("leticia", valor.get(1));
-    }
-    
-    @Test
-    public void testImAlive10Elements() {
-    	Rendezvous r = new RendezvousImpl();
-    	List<String> valor = r.whoIsAlive();
-    	for(int i = 0; i < 10;i++) {
+    	for(int i = 0; i < 10; i++) {
     		r.iAmAlive("Hola");		
     	}
     	
-    	valor = r.whoIsAlive();
+    	List <String> ElementList = r.whoIsAlive();
     	
     	for(int i = 0; i < 10;i++) {
-    	Assert.assertEquals("Hola", valor.get(i));	
+    	Assert.assertEquals("Hola", ElementList.get(i));	
     	}
     }
     
     @Test
-    public void testImAlive10DifferentElements() {
+    public void testImAliveManyElements() {
     	Rendezvous r = new RendezvousImpl();
-    	List<String> valor = r.whoIsAlive();
-    	String s = "0";
-    	for(int i = 0; i < 10;i++) {
-			r.iAmAlive(s);
-    		s = s + "0";
-    		valor = r.whoIsAlive();
-    		Assert.assertEquals(s, valor.get(i));	
+    	for(int i = 0; i < 10; i++) {
+    		String s = "Element " + i;
+    		r.iAmAlive(s);
+    		List <String> ElementList = r.whoIsAlive();
+    		Assert.assertEquals(s, ElementList.get(i));	
     	}
     }
-    
-    @Test
-    public void testwhoIsAliveNull()
-    {
+    @Test(expected = IllegalArgumentException.class)
+    public void testImAliveNullParameter() {
     	Rendezvous r = new RendezvousImpl();
-    	List<String> valor = r.whoIsAlive();
-    	Assert.assertEquals(null,valor);
+    	r.iAmAlive(null);
+    }
+   
+    @Test
+    public void testwhoIsAliveEmpty() {
+    	Rendezvous r = new RendezvousImpl();
+    	List<String> ElementList = r.whoIsAlive();
+    	Assert.assertTrue(ElementList.size() == 0);
     }
     
     @Test
-    public void testwhoIsAlive1Element() {
+    public void testwhoIsAliveSingleElement() {
     	Rendezvous r = new RendezvousImpl();
     	r.iAmAlive("OnlyElement");
     	Assert.assertEquals(1, r.whoIsAlive().size());
     	Assert.assertEquals("OnlyElement", r.whoIsAlive().get(0));
     }
     
-    public void testwhoIsAlive2Elements() {
-    	Rendezvous r = new RendezvousImpl();
-    	r.iAmAlive("1stElement");
-    	r.iAmAlive("2ndElement");
-    	Assert.assertEquals(2, r.whoIsAlive().size());
-    	Assert.assertEquals("1stElement", r.whoIsAlive().get(0));
-    	Assert.assertEquals("2ndElement", r.whoIsAlive().get(1));
-    }
-    
     @Test
-    public void testwhoIsAlive10Elements() {
+    public void testwhoIsAliveManyElements() {
     	Rendezvous r = new RendezvousImpl();
-    	for(int i = 0;i < 10;i++) {
+    	for(int i = 0; i < 10; i++) {
     		r.iAmAlive("Element" + (i+1));
     	}
     	
-    	for(int i = 0;i < 10;i++) {
+    	for(int i = 0; i < 10; i++) {
     		Assert.assertEquals("Element" + (i+1), r.whoIsAlive().get(i)); 
     	}
     }
