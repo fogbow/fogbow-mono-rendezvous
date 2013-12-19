8e1774fd4dbef1d0bac6a8f9303d3de1042c98b3
leticiamaia <leticiamt@hotmail.com>
2013-12-19 16:47:28 -0300

    Changed the Type of AliveIds from HashMap to ConcurrentHashMap.

---
+++ src/main/java/org/ourgid/cloud/RendezvousItem.java
@@ -0,0 +1,17 @@
+package org.ourgid.cloud;
+
+public class RendezvousItem {
+	private long lastTime ;
+
+	public RendezvousItem () {
+		lastTime = System.currentTimeMillis();
+	}
+	
+	public long getLastTime() {
+		return lastTime;
+	}
+	
+	public void setLastTime(long lastTime) {
+		this.lastTime = lastTime;
+	}
+}
