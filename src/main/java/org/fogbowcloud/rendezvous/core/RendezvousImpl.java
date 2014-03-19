721033858bc14730bc5f14d6af207cb2719d9620
fgan1 <fgan1@hotmail.com>
2014-03-19 17:24:35 -0300

    Added logs.
    fixes #14

--- src/main/java/org/fogbowcloud/rendezvous/core/RendezvousImpl.java
+++ src/main/java/org/fogbowcloud/rendezvous/core/RendezvousImpl.java
@@ -9,67 +9,79 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.log4j.Logger;
+import org.jamppa.component.XMPPComponent;
+
 public class RendezvousImpl implements Rendezvous {
 
-    public static final long TIMEOUT_DEFAULT = 3 * 60 * 1000;
-    private static final long PERIOD = 50;
+	private static final Logger LOGGER = Logger.getLogger(RendezvousImpl.class);
 
-    private final long timeOut;
-    private final Timer timer = new Timer();
-    private final ConcurrentHashMap<String, RendezvousItem> aliveIDs = new ConcurrentHashMap<String, RendezvousItem>();
-    private boolean inError = false;
+	public static final long TIMEOUT_DEFAULT = 3 * 60 * 1000;
+	private static final long PERIOD = 50;
 
-    public RendezvousImpl(long timeOut) {
-        if (timeOut < 0) {
-            throw new IllegalArgumentException();
-        }
-        this.timeOut = timeOut;
-        collectsNotAlive();
-    }
+	private final long timeOut;
+	private final Timer timer = new Timer();
+	private final ConcurrentHashMap<String, RendezvousItem> aliveIDs = new ConcurrentHashMap<String, RendezvousItem>();
+	private boolean inError = false;
 
-    public RendezvousImpl() {
-        this(TIMEOUT_DEFAULT);
-    }
+	public RendezvousImpl(long timeOut) {
+		if (timeOut < 0) {
+			throw new IllegalArgumentException();
+		}
+		this.timeOut = timeOut;
+		collectsNotAlive();
+	}
 
-    public void iAmAlive(ResourcesInfo resourcesInfo) {
-        if (resourcesInfo == null) {
-            throw new IllegalArgumentException();
-        }
+	public RendezvousImpl() {
+		this(TIMEOUT_DEFAULT);
+	}
 
-        aliveIDs.put(resourcesInfo.getId(), new RendezvousItem(resourcesInfo));
-    }
+	public void iAmAlive(ResourcesInfo resourcesInfo) {
+		if (resourcesInfo == null) {
+			throw new IllegalArgumentException();
+		}
+		LOGGER.info("Receiving iAmAlive from '" + resourcesInfo.getId()
+				+ "': MemIdle : '" + resourcesInfo.getMemIdle()
+				+ "'; MemInUse : '" + resourcesInfo.getMemInUse()
+				+ "'; CpuIdle : '" + resourcesInfo.getCpuIdle()
+				+ "'; CPuInUse : '" + resourcesInfo.getCpuInUse() + "'");
+		aliveIDs.put(resourcesInfo.getId(), new RendezvousItem(resourcesInfo));
+	}
 
-    public List<RendezvousItem> whoIsAlive() {
-        return new ArrayList<RendezvousItem>(aliveIDs.values());
-    }
+	public List<RendezvousItem> whoIsAlive() {
+		LOGGER.debug("WhoISAlive done.");
+		return new ArrayList<RendezvousItem>(aliveIDs.values());
+	}
 
-    private void collectsNotAlive() {
-        timer.schedule(new TimerTask() {
+	private void collectsNotAlive() {
+		timer.schedule(new TimerTask() {
 
-            @Override
-            public void run() {
-                checkExpiredAliveIDs();
-            }
-        }, 0, PERIOD);
-    }
+			@Override
+			public void run() {
+				checkExpiredAliveIDs();
+			}
+		}, 0, PERIOD);
+	}
 
-    private void checkExpiredAliveIDs() {
-        Iterator<Entry<String, RendezvousItem>> iter = aliveIDs.entrySet()
-                .iterator();
-        while (iter.hasNext()) {
-            try {
-                Entry<String, RendezvousItem> entry = iter.next();
-                if ((entry.getValue()).getLastTime() + timeOut < System
-                        .currentTimeMillis()) {
-                    iter.remove();
-                }
-            } catch (ConcurrentModificationException e) {
-                inError = true;
-            }
-        }
-    }
+	private void checkExpiredAliveIDs() {
+		Iterator<Entry<String, RendezvousItem>> iter = aliveIDs.entrySet()
+				.iterator();
+		while (iter.hasNext()) {
+			try {
+				Entry<String, RendezvousItem> entry = iter.next();
+				if ((entry.getValue()).getLastTime() + timeOut < System
+						.currentTimeMillis()) {
+					iter.remove();
+					LOGGER.info(entry.getValue().getResourcesInfo().getId()
+							+ " expired.");
+				}
+			} catch (ConcurrentModificationException e) {
+				inError = true;
+			}
+		}
+	}
 
-    protected boolean getInError() {
-        return inError;
-    }
+	protected boolean getInError() {
+		return inError;
+	}
 }
