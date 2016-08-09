e0ce2a54945ff57e33d7a5cbbc123258a38f7c1f
marcosnobrega <marcosnobregajr@gmail.com>
2016-08-09 15:22:56 -0300

    Added default methods to initialize AcceptAny plugin with and without properties

--- src/main/java/org/fogbowcloud/rendezvous/core/plugins/whitelist/AcceptAnyWhiteListPlugin.java
+++ src/main/java/org/fogbowcloud/rendezvous/core/plugins/whitelist/AcceptAnyWhiteListPlugin.java
@@ -1,11 +1,17 @@
 package org.fogbowcloud.rendezvous.core.plugins.whitelist;
 
+import java.util.Properties;
+
 import org.fogbowcloud.rendezvous.core.plugins.WhiteListPlugin;
 
 /**
  * Created by thiagoepdc on 10/05/16.
  */
 public class AcceptAnyWhiteListPlugin implements WhiteListPlugin {
+	
+	public AcceptAnyWhiteListPlugin() {}
+	
+	public AcceptAnyWhiteListPlugin(Properties properties) {}
 
     @Override
     public boolean contains(String memberId) {
